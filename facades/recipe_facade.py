8caa55b8333777de4490625c39cf5a3b0163756f
Silvia <sarnaizgarcia@gmail.com>
2020-01-16 13:29:42 +0100

    recipes facade refactor fixed

--- facades/recipe_facade.py
+++ facades/recipe_facade.py
@@ -13,26 +13,25 @@ class RecipeFacade():
             return False
         if len(name) < 1:
             return False
+        return True
 
     def _name_validation(self, name):
-        _validates_string_exists(name)
-        return True
+        return _validates_string_exists(name)
 
     def _ingredients_validation(self, ingredients):
         regex = r'(\w+\s\w+)'
         for ingredient in ingredients:
             if ingredient.get('name') == None or ingredient.get('quantity') == None:
                 return False
-            _validates_string_exists(ingredient['name'])
-            _validates_string_exists(ingredient['quantity'])
+            if not (_validates_string_exists(ingredient['name']) and _validates_string_exists(ingredient['quantity'])):
+                return False
             if not re.match(regex, item['quantity']):
                 return False
 
             return True
 
     def _instruction_validation(self, instructions):
-        _validates_string_exists(instructions)
-        return True
+        return _validates_string_exists(instructions)
 
     def _photo_validation(self, photo):
         regex = r'^data:image\/.*'
