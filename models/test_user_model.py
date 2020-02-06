1ba4b7f87bc8ae42b11c3e403d1681c946a59e72
Adrian Ferreres <adrian.ferreres@sngular.com>
2020-02-06 10:27:14 +0100

    Add unit test for save method in userModel
    - Remove class test_models
    - Add method test_user_save to test save method

--- models/test_user_model.py
+++ models/test_user_model.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch, MagicMock
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,6 +9,7 @@ from models.recipe_model import RecipeModel
 from models.ingredients_recipes_model import IngredientsRecipesModel
 from models.ingredients_model import IngredientsModel
 from db import db
+from logs import Logger
 
 
 class TestUserModel (unittest.TestCase):
@@ -20,13 +22,40 @@ class TestUserModel (unittest.TestCase):
 
     def setUp(self):
         db.metadata.create_all(self.engine)
-        self.session.add(UserModel(self.email, self.password))
-        self.session.commit()
 
     def tearDown(self):
         db.metadata.drop_all(self.engine)
 
     def test_user_creation(self):
+        self.session.add(UserModel(self.email, self.password))
+        self.session.commit()
         result = self.session.query(UserModel).all()
         self.assertEqual(result[0].email, self.email)
         self.assertEqual(result[0].password, self.password)
+
+    @patch('logs.Logger.__init__', lambda x, y: None)
+    @patch('logs.Logger.debug', autospec=True)
+    @patch('db.db.session.add', autospec=True)
+    @patch('db.db.session.commit', autospec=True)
+    def test_user_save(self, mockCommit, mockAdd, mockDebug):
+        userModel = UserModel(self.email, self.password)
+        userModel.save()
+
+        self.assertEqual(
+            mockDebug.call_args[0][1],
+            'Starting save user in database'
+        )
+        userSaved = mockAdd.call_args[0][0]
+
+        self.assertEqual(
+            userSaved.email,
+            self.email
+        )
+        self.assertEqual(
+            userSaved.password,
+            self.password
+        )
+        self.assertEqual(
+            mockCommit.call_count,
+            1
+        )
